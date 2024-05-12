49d4a1c6d43b9506243f4f46634288f6d56ec111
Dimas <dimasivantsov955@gmail.com>
2024-05-12 14:42:55 +0300

    123

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+ return (
+    <>
+    <div id='hello'><h1>Hello React</h1></div>
+
+    <div id='spisok'><ol>
+      <li>Полнедельник</li>
+      <li>Вторник</li>
+      <li>Среда</li>
+      <li>Четверг</li>
+      <li>Пятница</li>
+    </ol>
+    </div>
+    </>
+  );
+}
+
+export default App;
